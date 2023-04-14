8e4e01a3f8d713f2ceec95d1af7a69d85f50e4a6
zengrey <71875412+zengrey@users.noreply.github.com>
2023-04-15 03:22:44 +0800

    2023/4/15

--- Project/Assets/Scripts/Character/PlayerAim.cs
+++ Project/Assets/Scripts/Character/PlayerAim.cs
@@ -7,7 +7,10 @@ using UnityEngine;
 public class PlayerAim : MonoBehaviour
 {
     private Transform aimtransform;
-    public GameObject bullet;
+    public GameObject bullet1;
+    public GameObject bullet2;
+    public GameObject bullet3;
+    public GameObject bullet4;
     private Rigidbody2D bulletrb;
     Rigidbody2D rigidbody2d;
     private Vector2 offset;
@@ -19,20 +22,34 @@ public class PlayerAim : MonoBehaviour
     public int mode;
     private bool flag = true;
     private float nextfire=0f;
-    //setting the sword
-
+    //setting the weapon
+    int totalweapon = 2;
+    public int currentweaponindex;
+    public GameObject[] guns;
+    public GameObject gunholder;
+    public GameObject currentGun;
+    string[] Gunlist = { "normalStaff","shotgunStaff","bombStaff","machineGunStaff"};
     float angle;
     // Start is called before the first frame update
     private void Awake()
     {
-        aimtransform = transform.Find("Aim");
         rigidbody2d = GetComponent<Rigidbody2D>();
+        guns=new GameObject[4];
+        for(int i=0; i<4; i++)
+        {
+            guns[i] = transform.GetChild(i).gameObject;
+            guns[i].SetActive(false);
+        }
+        guns[0].SetActive(true);
+        aimtransform = transform.Find(Gunlist[0]);
     }
 
     // Update is called once per frame
     void Update()
     {
+        changegun();
         HandleAim();   
+       
             if (mode == 0)
             {
                 NormalShoot();
@@ -48,7 +65,7 @@ public class PlayerAim : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
         offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
-        angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg-45.0f;
         aimtransform.rotation = Quaternion.Euler(0, 0, angle);
     }
     private void NormalShoot()
@@ -57,7 +74,9 @@ public class PlayerAim : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && Time.time >nextfire)
         {
             nextfire = Time.time + firerate;
-            GameObject bullets = Instantiate(bullet, aimtransform.position,aimtransform.rotation);
+            float realangle = angle + 45.0f;
+            Quaternion rotation= Quaternion.Euler(0, 0, realangle);
+            GameObject bullets = Instantiate(bullet, aimtransform.position,rotation);
             bulletrb = bullets.GetComponent<Rigidbody2D>();
             bulletrb.velocity = bullets.transform.right * speed;
             Destroy(bullets,range);
@@ -70,12 +89,49 @@ public class PlayerAim : MonoBehaviour
         if (Input.GetMouseButton(0) && Time.time > nextfire)
         {
             nextfire = Time.time + firerate;
-            GameObject bullets = Instantiate(bullet, aimtransform.position, aimtransform.rotation);
+            float realangle = angle + 45.0f;
+            Quaternion rotation = Quaternion.Euler(0, 0, realangle);
+            GameObject bullets = Instantiate(bullet, aimtransform.position, rotation);
             bulletrb = bullets.GetComponent<Rigidbody2D>();
             bulletrb.velocity = bullets.transform.right * speed;
             Destroy(bullets, range);
         }
 
+    }
+    private void changegun()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            guns[0].SetActive(true);
+            aimtransform = transform.Find(Gunlist[0]);
+            guns[1].SetActive(false);
+            guns[2].SetActive(false);
+            guns[3].SetActive(false);
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            guns[1].SetActive(true);
+            aimtransform = transform.Find(Gunlist[1]);
+            guns[0].SetActive(false);
+            guns[2].SetActive(false);
+            guns[3].SetActive(false);
+        }
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            guns[2].SetActive(true);
+            aimtransform = transform.Find(Gunlist[2]);
+            guns[0].SetActive(false);
+            guns[1].SetActive(false);
+            guns[3].SetActive(false);
+        }
+        else if (Input.GetKey(KeyCode.Alpha4))
+        {
+            guns[3].SetActive(true);
+            aimtransform = transform.Find(Gunlist[3]);
+            guns[0].SetActive(false);
+            guns[1].SetActive(false);
+            guns[2].SetActive(false);
+        }
     }
 
 }
---
+++ Project/Assets/Scripts/Enemy/Enemyfollow.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using UnityEngine;
+
+public class Enemyfollow : MonoBehaviour
+{
+    public GameObject player;
+    private float distance;
+    public float speed;
+    public float health;
+    public float damage;
+    public float sight;
+    bool faceright = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if (distance < sight)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            if (transform.position.x > player.transform.position.x && faceright)
+            {
+                Flip();
+            }
+            else if (transform.position.x < player.transform.position.x && !faceright)
+            {
+                Flip();
+            }
+            //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+    }
+    void Flip()
+    {
+        Vector3 currentScale= gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        faceright = !faceright;
+    }
+
+}
