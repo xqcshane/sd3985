d8000c0a0f13f1240bb4b029c6a6560be3898db0
zengrey <71875412+zengrey@users.noreply.github.com>
2023-04-25 21:26:34 +0800

    2023/4/25 21：30

--- Project/Assets/Scripts/Managers/Passage/Passage.cs
+++ Project/Assets/Scripts/Managers/Passage/Passage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Passage : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Vector3 newpos;
     void Start()
     {
         
@@ -19,22 +20,28 @@ public class Passage : MonoBehaviour
     {
         Debug.Log("haha");
         if (collision.gameObject.CompareTag("Player")){
-            if (collision.GetComponent<NonetworkMove>().inpassage == false)
+            /* if (collision.GetComponent<NonetworkMove>().inpassage == false)
+             {
+                 collision.gameObject.GetComponent<NonetworkMove>().inpassage =true;
+                 collision.gameObject.GetComponent<NonetworkMove>().speed += 0.2f;
+                 collision.gameObject.GetComponent<NonetworkAim>().enabled = false;
+             }
+             else if(collision.GetComponent<NonetworkMove>().inpassage == true)
+             {
+                 collision.gameObject.GetComponent<NonetworkMove>().inpassage = false;
+                 collision.gameObject.GetComponent<NonetworkMove>().speed -= 0.2f;
+                 collision.gameObject.GetComponent<NonetworkAim>().enabled = true;
+                 GameObject[] allenemy=GameObject.FindGameObjectsWithTag("Enemy");
+                 for(int i = 0; i < allenemy.Length; i++)
+                 {
+                     allenemy[i].GetComponent<NonetworkEnemy>().enterpassage = false;
+                 }
+             }*/
+            collision.gameObject.transform.position = newpos;
+            GameObject[] allenemy = GameObject.FindGameObjectsWithTag("Enemy");
+            for (int i = 0; i < allenemy.Length; i++)
             {
-                collision.gameObject.GetComponent<NonetworkMove>().inpassage =true;
-                collision.gameObject.GetComponent<NonetworkMove>().speed += 0.2f;
-                collision.gameObject.GetComponent<NonetworkAim>().enabled = false;
-            }
-            else if(collision.GetComponent<NonetworkMove>().inpassage == true)
-            {
-                collision.gameObject.GetComponent<NonetworkMove>().inpassage = false;
-                collision.gameObject.GetComponent<NonetworkMove>().speed -= 0.2f;
-                collision.gameObject.GetComponent<NonetworkAim>().enabled = true;
-                GameObject[] allenemy=GameObject.FindGameObjectsWithTag("Enemy");
-                for(int i = 0; i < allenemy.Length; i++)
-                {
-                    allenemy[i].GetComponent<NonetworkEnemy>().enterpassage = false;
-                }
+                allenemy[i].GetComponent<NonetworkEnemy>().enterpassage = false;
             }
         }
         if (collision.gameObject.CompareTag("Enemy"))
