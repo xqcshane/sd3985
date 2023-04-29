beb78c036f36694abef553354d1d495ac47f0385
zengrey <71875412+zengrey@users.noreply.github.com>
2023-04-29 19:31:32 +0800

    Squashed commit of the following:
    commit 4f2f546968078daf042afdc109a21986c64f05ea
    Author: xqcshane <83743200+xqcshane@users.noreply.github.com>
    Date:   Sat Apr 29 19:19:41 2023 +0800
    
        RANDOM ROOM AND  ENEMY

---
+++ Project/Assets/Scripts/Rooms/RandomEmeny.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEmeny : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefab;
+    public int EnemyNumber;
+    void Start()
+    {
+        StartRandom();
+        //mypre.transform.position = new Vector3(Random.Range(-rmx, rmx), Random.Range(-rmy, rmy),0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartRandom(){
+        for(int i=0;i<EnemyNumber;i++){
+            float rmx = this.gameObject.GetComponent<BoxCollider2D>().bounds.size.x/2;
+            float rmy = this.gameObject.GetComponent<BoxCollider2D>().bounds.size.y/2;
+
+            Vector3 position = new Vector3(Random.Range(-rmx, rmx), Random.Range(1-rmy, rmy-1),0);
+            GameObject mypre = Instantiate(prefab, position+this.gameObject.transform.position, Quaternion.identity);
+            mypre.transform.parent = this.gameObject.transform;
+        }
+    }
+}
